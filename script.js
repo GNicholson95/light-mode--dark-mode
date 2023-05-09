f5fc1ccf78d08f846875d27b98d2cf853d22188c
George Nicholson <ga.nicholson@outlook.com>
2023-05-09 21:15:48 +0100

    added local storage functionality to remember theme preference

--- script.js
+++ script.js
@@ -37,9 +37,11 @@ const lightMode =()=>{
 function switchTheme(event){
     if (event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('theme', 'dark');
         darkMode()
     }else{
         document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('theme', 'light');
         lightMode()
     }
 }
@@ -47,3 +49,15 @@ function switchTheme(event){
 // Event listener
 
 toggleSwitch.addEventListener('change', switchTheme)
+
+// Check local storage for theme
+
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
